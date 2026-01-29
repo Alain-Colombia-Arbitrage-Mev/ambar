ee8221883b3375da505efb2fb82cecbd0b1cc8a2
Studio Ambar <studio@ambar.com>
2026-01-29 02:53:08 -0500

    fix: replace buggy sitemap plugin with static sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://studioambar.com',
   output: 'static',
   integrations: [
-    sitemap(),
     tailwind(),
   ],
   compressHTML: true,
