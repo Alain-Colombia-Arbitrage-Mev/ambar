c01bd33c795d85c53a9a6b5cf96ef3e44f039b49
Studio Ambar <studio@ambar.com>
2026-01-29 02:51:21 -0500

    fix: remove Cloudflare adapter for static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://studioambar.com',
   output: 'static',
-  adapter: cloudflare({
-    imageService: 'passthrough',
-  }),
   integrations: [
     sitemap(),
     tailwind(),
